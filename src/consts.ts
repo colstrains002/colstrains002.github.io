a244dfaab6b11301907333784aabf16328a37723
HirokiTakah <colstrains@gmail.com>
2026-01-04 00:11:45 +0900

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "dai era";
-export const SITE_DESCRIPTION = "ようこそ!";
+export const SITE_TITLE = "Colstrains";
+export const SITE_DESCRIPTION =
+  "音楽・創作・日々のメモ / Notes on music, art, and everyday life.";
